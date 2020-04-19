f73705356afbb7f33329c09018c3e0adfd22b5bb
stanislasbonifetto <stanislas.bonifetto@gmail.com>
2020-04-19 17:14:04 +0100

    Integration kafka producer (#2)
    * create integration test folder and unit test
    
    * add instruction how to run integration tests
    
    * add the kafka lib
    
    * forgot to add the cabal
    
    * add instruction how to install kafka C lib
    
    * add bytestring dependency
    
    Co-authored-by: Stefano Debenedetti <44903077+demaledetti@users.noreply.github.com>

--- test/integration/Spec.hs
+++ test/integration/Spec.hs
@@ -1,2 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Control.Exception (bracket)
+import Control.Monad (forM_)
+import Data.ByteString (ByteString)
+import Kafka.Producer
+
 main :: IO ()
-main = putStrLn "Integration Test suite not yet implemented"
+main = runProducerExample
+
+-- Global producer properties
+producerProps :: ProducerProperties
+producerProps = brokersList [BrokerAddress "localhost:9092"]
+             <> logLevel KafkaLogDebug
+
+-- Topic to send messages to
+targetTopic :: TopicName
+targetTopic = TopicName "kafka-client-example-topic"
+
+-- Run an example
+runProducerExample :: IO ()
+runProducerExample =
+    bracket mkProducer clProducer runHandler >>= print
+    where
+      mkProducer = newProducer producerProps
+      clProducer (Left _)     = return ()
+      clProducer (Right prod) = closeProducer prod
+      runHandler (Left err)   = return $ Left err
+      runHandler (Right prod) = sendMessages prod
+
+sendMessages :: KafkaProducer -> IO (Either KafkaError ())
+sendMessages prod = do
+  err1 <- produceMessage prod (mkMessage Nothing (Just "test from producer") )
+  forM_ err1 print
+
+  err2 <- produceMessage prod (mkMessage (Just "key") (Just "test from producer (with key)"))
+  forM_ err2 print
+
+  return $ Right ()
+
+mkMessage :: Maybe ByteString -> Maybe ByteString -> ProducerRecord
+mkMessage k v = ProducerRecord
+                  { prTopic = targetTopic
+                  , prPartition = UnassignedPartition
+                  , prKey = k
+                  , prValue = v
+                  }
