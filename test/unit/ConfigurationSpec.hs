d4637d046583f64d635d093d857792f3ccde06d1
Stanislas Bonifetto <stanislas.bonifetto@gmail.com>
2020-04-21 21:56:31 +0100

    add unit tests for topics

--- test/unit/ConfigurationSpec.hs
+++ test/unit/ConfigurationSpec.hs
@@ -7,9 +7,15 @@ import Kafka.Types
 
 spec :: Spec
 spec = do
-  describe "local" $ do
+  describe "local configuration" $ do
     it "name" $ do
       name local `shouldBe` "local"
     
     it "brokers" $ do
       brokers local `shouldBe` [BrokerAddress "localhost:9092"]
+    
+    it "topicFrom" $ do
+      topicFrom local `shouldBe` TopicName "topic-a"
+    
+    it "topicTo" $ do
+      topicTo local `shouldBe` TopicName "topic-b"
