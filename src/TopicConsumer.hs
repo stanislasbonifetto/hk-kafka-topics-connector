8645514f2b1709e18446ca4643840a3dfe43a643
stanislasbonifetto <stanislas.bonifetto@gmail.com>
2020-04-22 23:38:59 +0100

    Build dummy consumer (#6)
    * create a dummy consumer
    
    * create the broker configuration
    
    * refactoring the topic in the configuration
    
    * add unit tests for topics

---
+++ src/TopicConsumer.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+module TopicConsumer
+    ( runTopicConsumer
+    ) where
+
+import Control.Exception (bracket)
+import Data.Monoid ((<>))
+import Kafka.Consumer
+import Configuration
+
+-- Global consumer properties
+consumerProps :: ConsumerProperties
+consumerProps = brokersList (brokers local)
+             <> groupId (ConsumerGroupId "consumer_example_group")
+             <> noAutoCommit
+             <> logLevel KafkaLogInfo
+
+-- Subscription to topics
+consumerSub :: Subscription
+consumerSub = topics [topicFrom local]
+           <> offsetReset Latest
+
+-- Running consumer
+runTopicConsumer :: IO ()
+runTopicConsumer = do
+    res <- bracket mkConsumer clConsumer runHandler
+    print res
+    where
+      mkConsumer = newConsumer consumerProps consumerSub
+      clConsumer (Left err) = return (Left err)
+      clConsumer (Right kc) = maybe (Right ()) Left <$> closeConsumer kc
+      runHandler (Left err) = return (Left err)
+      runHandler (Right kc) = processMessages kc
+
+-------------------------------------------------------------------
+processMessages :: KafkaConsumer -> IO (Either KafkaError ())
+processMessages kafka = do
+    msg1 <- pollMessage kafka (Timeout 1000)
+    putStrLn $ "Message: " <> show msg1
+    err <- commitAllOffsets OffsetCommit kafka
+    putStrLn $ "Offsets: " <> maybe "Committed." show err
+    processMessages kafka
+    return $ Right ()    
--- test/unit/ConfigurationSpec.hs
+++ test/unit/ConfigurationSpec.hs
@@ -1,13 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module ConfigurationSpec where
 
 import Test.Hspec
 import Configuration
+import Kafka.Types
 
 spec :: Spec
 spec = do
-  describe "local" $ do
+  describe "local configuration" $ do
     it "name" $ do
       name local `shouldBe` "local"
     
     it "brokers" $ do
-      brokers local `shouldBe` ["localhost:9092"]
+      brokers local `shouldBe` [BrokerAddress "localhost:9092"]
+    
+    it "topicFrom" $ do
+      topicFrom local `shouldBe` TopicName "topic-a"
+    
+    it "topicTo" $ do
+      topicTo local `shouldBe` TopicName "topic-b"
